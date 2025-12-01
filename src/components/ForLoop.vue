c5275dc5e7428d7515bca69c0cf07b467845906f
Princekrcoder <princekrcoder@gmail.com>
2025-12-01 20:03:08 +0530

    auto-sync: changes at 01-12-2025 20:03:08.20

--- src/components/ForLoop.vue
+++ src/components/ForLoop.vue
@@ -1,7 +1,14 @@
 <template>
     <div>
-        <h1></h1>
+        <h1>Lecture - 14</h1>
+        <h1>For Loop Component</h1>
+
+
     </div>
 </template>
-<script></script>
+
+<script>
+
+</script>
+
 <style scoped></style>
