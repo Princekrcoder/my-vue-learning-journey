0c6c6c00991cd2b63428e7864b796716d99d10e0
Princekrcoder <princekrcoder@gmail.com>
2025-12-01 20:19:41 +0530

    auto-sync: changes at 01-12-2025 20:19:41.23

--- src/components/ForLoop.vue
+++ src/components/ForLoop.vue
@@ -3,7 +3,7 @@
     <h1>Lecture - 14</h1>
     <h1>For Loop Component</h1>
     <ul>
-        <li v-for="i in techStack" :key="i.name"></li>
+        <li v-for="i in techStack" :key="i"></li>
     </ul>
 
 </div>
