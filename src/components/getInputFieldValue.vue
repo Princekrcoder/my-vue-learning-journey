88cf939de910f57c19d4e0c298d5a9f4cfe6c882
Princekrcoder <princekrcoder@gmail.com>
2025-12-01 18:21:26 +0530

    auto-sync: changes at 01-12-2025 18:21:26.29

--- src/components/getInputFieldValue.vue
+++ src/components/getInputFieldValue.vue
@@ -21,7 +21,9 @@ export default {
             password: ''
         },
         methods: {
-           getMetadata()  
+            getData() {
+                
+            }  
         }
     }
 
