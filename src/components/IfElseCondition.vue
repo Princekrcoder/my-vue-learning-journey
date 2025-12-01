89d99b0a325bc2720e3ddf2e134b04ba32b1ffc3
Princekrcoder <princekrcoder@gmail.com>
2025-12-01 19:15:40 +0530

    auto-sync: changes at 01-12-2025 19:15:40.26

--- src/components/IfElseCondition.vue
+++ src/components/IfElseCondition.vue
@@ -3,5 +3,14 @@
 <script></script>
 
 <style scoped>
-
+div {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    background-color: aqua;
+    margin:0px 0px 100px 0px;
+}
 </style>
