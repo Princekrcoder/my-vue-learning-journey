406b406365ac0bdbd9c6aadcde1050cb8d508683
Princekrcoder <princekrcoder@gmail.com>
2025-12-01 19:48:08 +0530

    auto-sync: changes at 01-12-2025 19:48:08.30

--- src/components/IfElseCondition.vue
+++ src/components/IfElseCondition.vue
@@ -2,7 +2,7 @@
 <div>
     <h1>Lecture - 13</h1>
     <h1>Ife Condition Component</h1>
-    <p v-if="A">This is true Condition</p>
+    <!-- <p v-if="A">This is true Condition</p>
 
     <h1>If-Else Condition Component</h1>
     <p v-if="B">This is true Condition</p>
@@ -11,7 +11,7 @@
     <h1>If-Else Condition Component</h1>
     <p v-if="C">This is true Condition</p>
     <p v-else-if="D">This is else-if Condition</p>
-    <p v-else>This is else Condition</p>
+    <p v-else>This is else Condition</p> -->
 
     
 </div>
