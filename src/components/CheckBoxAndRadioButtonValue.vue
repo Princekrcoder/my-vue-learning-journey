50c507e5717ec34a2e5692e396bd67bd9ddc5213
Princekrcoder <princekrcoder@gmail.com>
2025-12-01 18:58:10 +0530

    auto-sync: changes at 01-12-2025 18:58:10.19

--- src/components/CheckBoxAndRadioButtonValue.vue
+++ src/components/CheckBoxAndRadioButtonValue.vue
@@ -3,7 +3,8 @@
         <h1>Lecture - 12</h1>
         <h1>CheckBox and Radio Button Value</h1>
 
-        <label for=""></label>
+        <label for="java">java</label>
+        inp
     </div>
 </template>
 
