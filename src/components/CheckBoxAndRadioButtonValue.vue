67f761f9f88867b25a0d2a40aa6af8f30b113b1b
Princekrcoder <princekrcoder@gmail.com>
2025-12-01 18:39:06 +0530

    auto-sync: changes at 01-12-2025 18:39:06.28

--- src/components/CheckBoxAndRadioButtonValue.vue
+++ src/components/CheckBoxAndRadioButtonValue.vue
@@ -19,13 +19,14 @@ export default {
 </script>
 
 <style scoped>
-body {
+div {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+    background-color: aqua;
 }
 
 </style>
