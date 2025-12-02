920578733df6bb47e013d7f09dff10606a2f4fca
Princekrcoder <princekrcoder@gmail.com>
2025-12-03 00:34:31 +0530

    auto-sync: changes at 03-12-2025  0:34:31.23

--- src/components/PassDataToChild.vue
+++ src/components/PassDataToChild.vue
@@ -2,7 +2,9 @@
   <div>
     <h1>Lecture - 15</h1>
     <h1>Pass Data To Child Component</h1>
+    <h1>{{ us.name }}</h1>
     <h1>{{ name }}</h1>
+    <button v-on:click="al()" type="button">Show Alert</button>
 
     
   </div>
