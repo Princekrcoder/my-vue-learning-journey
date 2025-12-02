ae82e49722d02d995e721ee3decb8823656fd17b
Princekrcoder <princekrcoder@gmail.com>
2025-12-03 00:39:35 +0530

    auto-sync: changes at 03-12-2025  0:39:35.22

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     <CheckBoxAndRadioButtonValue />
     <IfElseCondition />
     <ForLoop />
-    <PassDataToChild />
+    
     
 </div>
 </template>
@@ -31,7 +31,7 @@ export default {
         CheckBoxAndRadioButtonValue,
         IfElseCondition,
         ForLoop,
-        PassDataToChild
+        
     }
 }
 </script>
