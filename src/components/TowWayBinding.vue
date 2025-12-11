09eeefc5d9f730070a84ec64ef04fafd12b17c28
Princekrcoder <princekrcoder@gmail.com>
2025-12-11 10:01:40 +0530

    auto-sync: changes at 11-12-2025 10:01:40.07

---
+++ src/components/TowWayBinding.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <h1>Two Way Binding</h1>
+    <input type="text" v-model="count" />
+    <h1>{{ count }}</h1>
+    <br>
+    <hr>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'TwoWayBinding',
+    data() {
+        return {
+            count: ``
+        }
+    }
+
+}
+</script>
