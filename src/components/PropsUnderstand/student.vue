5e6712c14e2ce3eb9ed652dd483fca4f0dfc5bd1
Princekrcoder <princekrcoder@gmail.com>
2025-12-13 02:21:21 +0530

    auto-sync: changes at 13-12-2025  2:21:21.27

--- src/components/PropsUnderstand/student.vue
+++ src/components/PropsUnderstand/student.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
        <h3>Student data</h3>
+       <h3>Student name is </h3>
     </div>
 </template>
 
 <script>
     export default {
         name: 'StuDent',
-        
+        props: {
+            name: String
+        }
     }
 </script>
