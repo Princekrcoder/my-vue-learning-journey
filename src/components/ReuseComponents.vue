ddb10ae9c5321acf97e028b2367e86153659b351
Princekrcoder <princekrcoder@gmail.com>
2025-12-12 07:37:48 +0530

    auto-sync: changes at 12-12-2025  7:37:48.30

--- src/components/ReuseComponents.vue
+++ src/components/ReuseComponents.vue
@@ -33,8 +33,8 @@ export default {
         }
     },
     methods: {
-            alertName() {
-                alert(users.name);
+            alertName(user) {
+                alert(user.name);
                 
             }
         }
