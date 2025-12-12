8e9f319e0501f2d6283c2393f78302fb43962880
Princekrcoder <princekrcoder@gmail.com>
2025-12-12 07:45:33 +0530

    auto-sync: changes at 12-12-2025  7:45:33.40

--- src/components/HTMLbinding.vue
+++ src/components/HTMLbinding.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
-        <h1>Lecture - </h1>
+        <h1>Lecture - 18</h1>
         <h1>This is HTML Binding Component</h1>
         
     </div>
 </template>
+
+<script>
+    export default {
+        name: 'HTMLbinding',
+        data() {
+            
+        }
+        
+    }
+</script>
