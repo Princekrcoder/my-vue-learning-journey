e5ed68ba6bfa824aadf92dd386254eb268a8ecfe
Princekrcoder <princekrcoder@gmail.com>
2025-12-13 02:17:02 +0530

    auto-sync: changes at 13-12-2025  2:17:02.30

--- src/components/PropsUnderstand/prop.vue
+++ src/components/PropsUnderstand/prop.vue
@@ -2,12 +2,14 @@
     <div>
         <h1>Lecture - 20</h1>
         <h2>Understanding Props in Vue.js</h2>
+        
 
     </div>
 </template>
 
 <script>
-    im
+    import Student from './student.vue'
+    import Teacher from './teacher.vue'
     export default {
         name: 'PropsUnderstand',
     }
