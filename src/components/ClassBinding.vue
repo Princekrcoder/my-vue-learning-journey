673595ac6eaf1210e797cab9d246da05f415b73f
Princekrcoder <princekrcoder@gmail.com>
2025-12-13 01:54:18 +0530

    auto-sync: changes at 13-12-2025  1:54:18.29

--- src/components/ClassBinding.vue
+++ src/components/ClassBinding.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Lecture - 19</h1>
-        <h2 class="normal" :class="{clbinding:bgcolor,err:}">Class Binding in Vue.js</h2>
+        <h2 class="normal" :class="{clbinding:bgcolor,err:true}">Class Binding in Vue.js</h2>
         <button v-on:click="bgcolor=!bgcolor">Apply Style</button>
 
     </div>
@@ -17,7 +17,9 @@
         },
         computed: {
             applyStyle() {
-
+                return {
+                    
+                }
             }
         }
     }
