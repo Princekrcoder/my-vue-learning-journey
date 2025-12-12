7b57f2218907be7c444c3aae6f7435bacb1dd182
Princekrcoder <princekrcoder@gmail.com>
2025-12-13 01:52:44 +0530

    auto-sync: changes at 13-12-2025  1:52:44.19

--- src/components/ClassBinding.vue
+++ src/components/ClassBinding.vue
@@ -15,7 +15,11 @@
                 bgcolor: false
             }
         },
-        computed
+        computed: {
+            applyStyle() {
+                
+            }
+        }
     }
 </script>
 
