6049240ea8d247333ead576f8c8d202e5029df49
Princekrcoder <princekrcoder@gmail.com>
2025-12-13 02:19:10 +0530

    auto-sync: changes at 13-12-2025  2:19:10.35

--- src/components/PropsUnderstand/prop.vue
+++ src/components/PropsUnderstand/prop.vue
@@ -14,7 +14,7 @@
     export default {
         name: 'PropsUnderstand',
         components: {
-            Student,
+            StuDent,
             Teacher
         }
     }
--- src/components/PropsUnderstand/student.vue
+++ src/components/PropsUnderstand/student.vue
@@ -3,3 +3,9 @@
        <h3>Student data</h3>
     </div>
 </template>
+
+<script>
+    export default {
+        name: 'StuDent',
+    }
+</script>
