accb370729c74150261449653f3fc84b81523758
Princekrcoder <princekrcoder@gmail.com>
2025-12-14 20:17:55 +0530

    auto-sync: changes at 14-12-2025 20:17:55.20

--- src/components/SendChildToParent/parent.vue
+++ src/components/SendChildToParent/parent.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        <h1>Lecture </h1>
+        <h1>Lecture - 21</h1>
         <h1>Parent data</h1>
+
     </div>
 </template>
+
+<script>
+    export default {
+        name: 'ParentData',
+        
